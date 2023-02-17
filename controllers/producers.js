4f6f4c814df793248d6f60770f4d0763c79521bb
Patrick Mazara <patrickmazara@hotmail.com>
2023-02-17 02:18:01 -0500

    add update and delete functionality in producers controller

--- controllers/producers.js
+++ controllers/producers.js
@@ -9,6 +9,39 @@ const create = async (req, res) => {
   }
 }
 
+const index = async (req, res) => {
+  try {
+    const producer = await Producer.findAll()
+    res.status(200).json(producer)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+const update = async (req, res) => {
+  try {
+    const producer = await Producer.findByPk(req.params.id)
+    producer.set(req.body)
+    await producer.save
+    res.status(200).json(producer)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+const deleteProducer = async (req, res) => {
+  try {
+    const producer = await Producer.findByPk(req.params.id)
+    await producer.destroy()
+    res.status(200).json(producer)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
 module.exports = {
-  create
+  create,
+  index,
+  update,
+  delete: deleteProducer
 }
--- routes/producers.js
+++ routes/producers.js
@@ -4,8 +4,8 @@ const prodCtrl = require('../controllers/producers.js')
 // routes
 
 router.post('/', prodCtrl.create)
-// router.get('/', prodCtrl.index)
-// router.put('/:id', prodCtrl.update)
+router.get('/', prodCtrl.index)
+router.put('/:id', prodCtrl.update)
 // router.delete('/:id', prodCtrl.delete)
 // router.post('/:id/placements', prodCtrl.addPlacement)
 module.exports = router
