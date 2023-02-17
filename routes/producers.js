0d706ee52f98acab762b35bd6a507cc95d53b3c2
Patrick Mazara <patrickmazara@hotmail.com>
2023-02-17 01:55:04 -0500

    create route and controlller for producers, begin controller functionality

---
+++ routes/producers.js
@@ -0,0 +1,11 @@
+const router = require('express').Router()
+const prodCtrl = require('../controllers/producers.js')
+
+// routes
+
+router.post('/', prodCtrl.create)
+router.get('/', prodCtrl.index)
+router.put('/:id', prodCtrl.update)
+router.delete('/:id', prodCtrl.delete)
+router.post('/:id/feedings', prodCtrl.addFeeding)
+module.exports = router
